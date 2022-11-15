7df2ed54a5e15dd0ba7fa07f5ed8f5cea91a7d8d
kerime abanoz <krmeabnz@gmail.com>
2022-11-15 14:07:58 +0300

    break

--- js14_events/1-events-intro/events.js
+++ js14_events/1-events-intro/events.js
@@ -9,7 +9,7 @@ console.log("****** EVENTS *******");
 
 const h1 = document.querySelector("#header h1");
 
-//? 3 .method
+//? 3. method
 h1.onmouseover = function () {
   h1.style.color = "red";
   h1.style.fontWeight = "300";
@@ -51,7 +51,7 @@ document.getElementById("btn").addEventListener("click", function () {
 const list = document.querySelectorAll(".list"); //?nodelist
 
 list.forEach((li) => {
-  li.style.transition = "all 0.8s";
+  li.style.transition = "all 0.5s";
   li.style.lineHeight = "2rem";
 
   li.onmouseover = () => {
